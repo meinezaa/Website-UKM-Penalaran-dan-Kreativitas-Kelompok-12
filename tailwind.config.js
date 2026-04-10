c8c9e702bd071443a83cb15320152afc88514aef
Naila Sasikirana Gunawan <24082010063@student.upnjatim.ac.id>
2026-04-10 11:10:46 +0700

    update beranda

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./public/**/*.{html,js}",
-    "./src/**/*.{html,js}"
-  ],
+  content: ["./public/**/*.{html,js}", "./src/**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
-        poppins: ['Poppins', 'sans-serif'],
-        instrument: ['"Instrument Serif"', 'serif'],
+        poppins: ["Poppins", "sans-serif"],
+        instrument: ['"Instrument Serif"', "serif"],
       },
     },
   },
   plugins: [],
-}
+};
